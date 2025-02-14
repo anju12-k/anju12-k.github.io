21bad543cafcdb3914dcc927a2384062f621665b
anju12-k <anju.disney12@au.com>
2025-02-14 16:03:16 +0900

    Create slick-custom.js

---
+++ js/slick-custom.js
@@ -0,0 +1,12 @@
+$(function () {
+	$('.slide-item').slick({
+		autoplay: true,
+		autoplaySpeed: 2000,
+		speed: 2500,
+		dots: true,
+		fade: true,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+	});
+});
+
